4a1f172ea41aa2cabf9186a7f95985cc3b19d1b7
mohamed fathy <mohamed@fathy.com>
2025-10-01 00:14:21 +0200

    fixx

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+
+class Authenticate extends Middleware
+{
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     */
+    protected function redirectTo($request): ?string
+    {
+        if ($request->expectsJson()) {
+            abort(response()->json(['error' => 'Unauthenticated'], 401));
+        }
+
+        return route('admin.login');
+    }
+}
